b799d32843899c7fb19ba783321677a299263694
stuckfakur <benaleobayu@gmail.com>
2024-03-06 16:38:25 +0700

    mainan baru

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -20,6 +20,45 @@ buttonClose.addEventListener('click', () => {
 
 })
 
+// hover the bg
+const changeBg = document.getElementById('changeBg');
+const popupBg = document.querySelector('div.popupBg');
+changeBg.addEventListener('mouseover', ()=> {
+    popupBg.classList.add('show');
+})
+popupBg.addEventListener('mouseleave', ()=> {
+    popupBg.classList.remove('show');
+})
+// input the custom color bg
+let inputRed = document.getElementById('inputRed');
+let inputGreen = document.getElementById('inputGreen');
+let inputBlue = document.getElementById('inputBlue');
+
+function setBackgroundColor(){
+    let color = `rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value})`;
+    document.body.style.backgroundColor = color;
+    localStorage.setItem('backgroundColor', color);
+}
+function getSavedColor(){
+    let savedColor = localStorage.getItem('backgroundColor');
+    if (savedColor){
+        let [red, green, blue] = savedColor.match(/\d+/g);
+        inputRed.value = red;
+        inputGreen.value = green;
+        inputBlue.value = blue;
+
+        document.body.style.backgroundColor = `rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value})`;
+    }
+}
+
+inputRed.addEventListener('change', setBackgroundColor);
+inputGreen.addEventListener('change', setBackgroundColor);
+inputBlue.addEventListener('change', setBackgroundColor);
+
+getSavedColor();
+
+
+
 // js dark mode
 const checkbox = document.getElementById("checkbox-dark-mode")
 checkbox.addEventListener("change", () => {
