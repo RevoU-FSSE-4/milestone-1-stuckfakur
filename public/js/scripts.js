18f5d0e23ad471957908b58494acbfce668a5f3e
stuckfakur <benaleobayu@gmail.com>
2024-03-07 00:52:37 +0700

    portfolio

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,3 @@
-// use to toggle the sidebar
-const menu = document.querySelector('button[data-view="hamburger"]')
-const alertChild = document.querySelector('#alert');
-menu.addEventListener('click', () => {
-    const aside = document.querySelector('aside');
-    aside.classList.toggle('hidden');
-})
-// use to open popup alert notification after success submit form
-const formSubmit = document.getElementById('contactUs');
-formSubmit.addEventListener('submit', (e) => {
-    alertChild.classList.add('show-alert');
-    document.querySelector(".bg-blur").classList.add('show');
-    e.preventDefault();
-})
-// close popup alert notification
-const buttonClose = document.getElementById('closeBtn');
-buttonClose.addEventListener('click', () => {
-    alertChild.classList.remove('show-alert');
-    document.querySelector(".bg-blur").classList.remove('show');
-})
 
 // hover the bg
 const changeBg = document.getElementById('changeBg');
@@ -70,14 +50,11 @@ document.querySelector('button.reset').addEventListener('click', ()=>{
 
 // js dark mode
 const checkbox = document.getElementById("checkbox-dark-mode")
+
 checkbox.addEventListener("change", () => {
     localStorage.removeItem('backgroundColor');
     document.body.style.backgroundColor = '';
     changeBg.classList.toggle('opacity-0');
     setInputColorZero();
     document.body.classList.toggle("dark");
-
-    document.querySelectorAll('.popupBg label').forEach((e)=>{
-        e.classList.toggle('invert');
-    })
 })
